8ea896942207498779908bbd92f61723a79cfef9
chao-tiatde <s111219007@stu.ntue.edu.tw>
2025-05-27 14:06:55 +0800

    person

---
+++ src/components/UserInfo.jsx
@@ -0,0 +1,9 @@
+import {User} from 'lucide-react';
+import {useNavigate} from 'react-router';
+
+export default function UserInfo(props){
+    const navigate = useNavigate();
+    const goToProfile = () => {
+        
+    }
+}
