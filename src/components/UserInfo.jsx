7c18285db42ee75309621af412536393312dc9fd
chao-tiatde <s111219007@stu.ntue.edu.tw>
2025-06-06 11:19:27 +0800

    mobile person.svg

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -34,16 +34,13 @@ export default function UserInfo(props) {
       className="cursor-pointer flex flex-col items-center group"
     >
       <svg
-        width="15"
-        height="15"
         viewBox="0 0 25 25"
         fill="currentColor"
         xmlns="http://www.w3.org/2000/svg"
-        className={`${
-          lightMode
+        className={`w-[25px] h-[25px] md:w-[15px] md:h-[15px] ${lightMode
             ? 'text-black hover:text-[#7392B9]'
             : 'text-white hover:text-[#FAAA5D]'
-        }`}
+          }`}
       >
         <path d="M12.5 12.5C15.9531 12.5 18.75 9.70313 18.75 6.25C18.75 2.79687 15.9531 0 12.5 0C9.04687 0 6.25 2.79687 6.25 6.25C6.25 9.70313 9.04687 12.5 12.5 12.5ZM12.5 15.625C8.32812 15.625 0 17.7187 0 21.875V25H25V21.875C25 17.7187 16.6719 15.625 12.5 15.625Z" />
       </svg>
