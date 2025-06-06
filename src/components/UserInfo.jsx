eb5020c2d9d871de7221ba55068e8a6c8f70eefe
chao-tiatde <s111219007@stu.ntue.edu.tw>
2025-06-06 11:09:15 +0800

    person.svg

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
-import { auth } from '../api/firebaseConfig'; 
-import { onAuthStateChanged } from 'firebase/auth'; 
+import { auth } from '../api/firebaseConfig';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useSelector } from 'react-redux'; // 別忘了引入 useSelector
+import { selectLightMode } from "../redux/colorSlice.js";
 
 export default function UserInfo(props) {
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
+  const lightMode = useSelector(selectLightMode);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -30,7 +33,21 @@ export default function UserInfo(props) {
       style={{ ...props.style }}
       className="cursor-pointer flex flex-col items-center group"
     >
-      <img src="/img/person.png" className="w-[13px]" alt="user icon" />
+      <svg
+        width="15"
+        height="15"
+        viewBox="0 0 25 25"
+        fill="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
+        className={`${
+          lightMode
+            ? 'text-black hover:text-[#7392B9]'
+            : 'text-white hover:text-[#FAAA5D]'
+        }`}
+      >
+        <path d="M12.5 12.5C15.9531 12.5 18.75 9.70313 18.75 6.25C18.75 2.79687 15.9531 0 12.5 0C9.04687 0 6.25 2.79687 6.25 6.25C6.25 9.70313 9.04687 12.5 12.5 12.5ZM12.5 15.625C8.32812 15.625 0 17.7187 0 21.875V25H25V21.875C25 17.7187 16.6719 15.625 12.5 15.625Z" />
+      </svg>
+
       <p className="hidden md:block text-[0.5rem] md:text-[0.7rem] opacity-60 mt-[-0.2rem] md:mt-[0.3rem] text-current">
         {displayName ? `Hi, ${displayName}` : '請登入'}
       </p>
